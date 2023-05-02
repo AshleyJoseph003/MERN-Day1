9b90ff4c520554e65bd61acd181412a330acdafd
AshleyJoseph003 <132336342+AshleyJoseph003@users.noreply.github.com>
2023-05-02 15:48:26 +0530

    First

---
+++ script.js
@@ -0,0 +1,116 @@
+/*let a=10;
+let b=20;
+let c=a+b;
+let d=b-a;
+let e=a*b;
+let f=a/b;
+let g=a%b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);*/
+/*let a=10;
+let b="10";
+if(a==b)
+{console.log(true)
+}else{
+    console.log(false)
+}
+if(a===b)
+{console.log(true)
+}else{
+    console.log(false)
+}*/
+// here triple = will check the datatype along with the value equlity but double= will only check whether the values are same or not
+/*let f="ashley";
+let l="joseph";
+let fn=f+l;
+console.log(fn);*/
+/*let f="ashley";
+let l="joseph";
+let fn=f+l;
+console.log(`${f}  ${l}`);*/
+/*let car={
+    model:"celerio",
+    color:"white",
+    price:1000000
+}
+console.log(car);
+console.log(car.color);
+console.log(`my car is ${car.color} ${car.model}`)*/
+/*let person={
+    name:"sayu",
+    college:"christ",
+    class:"s4 cse",
+    address:{
+        housename:"xyz",
+        street:"trjytuj"
+    }}
+
+console.log(person);
+console.log(person.college);
+console.log(`${person.name} ${person.address.housename}`);*/
+// array
+/*let fruits=["apple","mango","orange"]
+fruits[3]="grapes";
+fruits.push("pineapple")
+fruits[5]="duiew";
+fruits.unshift("dragon")
+fruits.pop();
+fruits.shift();
+console.log(fruits);*/
+/*let students=[
+    {name:"ashley",age:20},
+    {name:"fhyhnjj",age:89}
+]
+console.log(students[1].name)
+console.log(students[0].age)*/
+/*function addTwoNumbers(a,b) {
+    console.log(a+b);
+}
+
+addTwoNumbers(10,20);
+function difference(a,b){
+    console.log(a-b);}
+difference(67,89);*/
+// add = (a,b) =>{
+//     console.log(a+b);
+// }
+
+// add(78,90);
+   
+// add = (a,b) =>{
+//     p =a+b;
+//     if(p>10)
+//     {console.log("the sum is greater than 10 ");}
+//     else{
+//         console.log("the sum is less than 10");
+//     }
+// }
+// add(60,50);
+
+// let numbers = [1,2,3,4,5,6,7,8,9];
+// console.log(numbers);
+
+// printArray=(array) =>{
+//     for(let i=0; i<array.length; i++)
+//     {console.log(array[i]);}
+// }
+//  printArray(numbers);
+
+let numbers = [1,2,3,4,5,6];
+console.log(numbers);
+printArray=(array) =>{let i=0;
+    while(i<array.length)
+    {
+        console.log(array[i]);
+    i++;}
+}
+printArray(numbers);
+
+
+
+
+
+   
